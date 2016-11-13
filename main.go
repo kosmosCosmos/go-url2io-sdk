590b5cb7482a06718edb455ad432aec1c831d8e3
kosmosCosmos <anasplrt34@gmail.com>
2016-11-13 14:58:42 +0800

    增加参数text,next

--- main.go
+++ main.go
@@ -13,8 +13,19 @@ str,_:=simplejson.NewJson(result)
 title, _ := str.Get("title").String()
 urls,_:=str.Get("url").String()
 date,_:=str.Get("date").String()
-	texts, _ := str.Get("text").String()
-	nexts, _ := str.Get("next").String()
-	getinfo = append(getinfo, urls, date, title, texts, nexts)
+	if text != nil {
+		texts, _ := str.Get("text").String()
+		getinfo = append(getinfo, texts)
+	}
+	if text == nil {
+		content := str.Get("content")
+		getinfo = append(getinfo, content)
+	}
+	if next != nil {
+		nexts, _ := str.Get("next").String()
+		getinfo = append(getinfo, nexts)
+	}
+	if next == nil {}
+	getinfo = append(getinfo, urls, date, title)
 	return getinfo
 }
