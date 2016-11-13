d50c367306cf051ad67a37744f5ea98c615f57b4
kosmosCosmos <anasplrt34@gmail.com>
2016-11-13 14:32:15 +0800

    增加参数text,next

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 	"github.com/bitly/go-simplejson"
 )
 
-func API(url string,token string)(getinfo []string) {
-reqest, _ := http.Get("http://api.url2io.com/article?token="+token+"&url="+url+"&fields=text")
+func API(url string, token string, text string, next string) (getinfo []string) {
+	reqest, _ := http.Get("http://api.url2io.com/article?token=" + token + "&url=" + url + "&fields=" + text + next)
 result, _ := ioutil.ReadAll(reqest.Body)
 str,_:=simplejson.NewJson(result)
 title, _ := str.Get("title").String()
 urls,_:=str.Get("url").String()
 date,_:=str.Get("date").String()
-text,_:=str.Get("text").String()
-next,_:=str.Get("next").String()
-getinfo=append(getinfo,urls,date,title,text,next)
+	texts, _ := str.Get("text").String()
+	nexts, _ := str.Get("next").String()
+	getinfo = append(getinfo, urls, date, title, texts, nexts)
 return getinfo
 }
