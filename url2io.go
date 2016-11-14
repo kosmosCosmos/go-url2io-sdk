78c03897552ed83196f813bbc7658f3623859f51
kosmosCosmos <kosmosCosmos@users.noreply.github.com>
2016-11-14 10:50:21 +0800

    Update url2io.go

--- url2io.go
+++ url2io.go
@@ -6,7 +6,7 @@ import (
 	"github.com/bitly/go-simplejson"
 )
 
-func API(url string, token string, text string, next string) (getinfo []string) {
+func API_article(url string, token string, text string, next string) (getinfo []string) {
 	reqest, _ := http.Get("http://api.url2io.com/article?token=" + token + "&url=" + url + "&fields=" + text + next)
 result, _ := ioutil.ReadAll(reqest.Body)
 str,_:=simplejson.NewJson(result)
